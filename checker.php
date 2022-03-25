59b151faa235daa28bf7f30b8b456dfbdaf99e9f
etymchina <etymchina@gmail.com>
2022-03-25 13:00:24 +0100

    New function isPangram

--- checker.php
+++ checker.php
@@ -56,6 +56,22 @@ class Checker
      */
     public function isPangram(string $phrase) : bool
     {
+        $alphabet = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+        $result = false;
+        $array = str_split($phrase);
+        foreach ($array as $char) {
+            if (ctype_upper($char)) {
+                $char = strtolower($char);
+            }
+            $key = array_search($char, $alphabet);
+            if ($key !== false) {
+                unset($alphabet[$key]);
+            }
+        }
+        if (!$alphabet) {
+            $result = true;
+        }
+        return $result;
     }
 }
 // $word = "anna";
@@ -66,12 +82,29 @@ class Checker
 //     echo $word ." is not a palindrome string";
 // }
 
-$word = "coalface";
-$comparison = "cacao elf";
+// $word = "coalface";
+// $comparison = "cacao elf";
 
+// $x = new Checker();
+// if ($x->isAnagram($word, $comparison)) {
+//     echo $word . " and " . $comparison . " are anagram";
+// } else {
+//     echo $word . " and " . $comparison . " are not anagram";
+// }
+$phrase = "The quick brown fox jumps over the lazy dog";
 $x = new Checker();
-if ($x->isAnagram($word, $comparison)) {
-    echo $word . " and " . $comparison . " are anagram";
+
+if ($x->isPangram($phrase)) {
+    echo "The phrase is pangram\n";
 } else {
-    echo $word . " and " . $comparison . " are not anagram";
+    echo "The phrase is not pangram\n";
+}
+
+$phrase = "The British Broadcasting Corporation (BBC) is a British public service broadcaster.";
+$x = new Checker();
+
+if ($x->isPangram($phrase)) {
+    echo "The phrase is pangram\n";
+} else {
+    echo "The phrase is not pangram\n";
 }
