98665521022b17262e03ccb4afb1cf14adc0037a
etymchina <etymchina@gmail.com>
2022-03-25 12:00:07 +0100

    New function isPalindrome

--- checker.php
+++ checker.php
@@ -4,14 +4,14 @@ namespace App;
 
 /**
  * Pangrams, anagrams and palindromes
- * 
- * Implement each of the functions of the Checker class. 
- * Aim to spend about 10 minutes on each function. 
+ *
+ * Implement each of the functions of the Checker class.
+ * Aim to spend about 10 minutes on each function.
  */
 class Checker
 {
     /**
-     * A palindrome is a word, phrase, number, or other sequence of characters 
+     * A palindrome is a word, phrase, number, or other sequence of characters
      * which reads the same backward or forward.
      *
      * @param string $word
@@ -19,13 +19,19 @@ class Checker
      */
     public function isPalindrome(string $word) : bool
     {
+        $reverseString = strrev($word);
+        if ($reverseString == $word) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
     /**
-     * An anagram is the result of rearranging the letters of a word or phrase 
-     * to produce a new word or phrase, using all the original letters 
+     * An anagram is the result of rearranging the letters of a word or phrase
+     * to produce a new word or phrase, using all the original letters
      * exactly once.
-     * 
+     *
      * @param string $word
      * @param string $comparison
      * @return bool
@@ -35,13 +41,21 @@ class Checker
     }
 
     /**
-     * A Pangram for a given alphabet is a sentence using every letter of the 
-     * alphabet at least once. 
-     * 
+     * A Pangram for a given alphabet is a sentence using every letter of the
+     * alphabet at least once.
+     *
      * @param string $phrase
      * @return bool
-     */    
+     */
     public function isPangram(string $phrase) : bool
     {
     }
+}
+$word = "anna";
+
+$x = new Checker();
+if ($x->isPalindrome($word)) {
+    echo $word ." is a palindrome string";
+} else {
+    echo $word ." is not a palindrome string";
 }
